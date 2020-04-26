8e77520946b4474d7b034f50b5aefdcbf019f41f
Shunta Komatsu <shuntak217@gmail.com>
2020-04-26 14:21:35 +0900

    :sparkles: Add icon to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import { AppBar, Button, Toolbar, Typography } from "@material-ui/core"
 
+import FileCopyIcon from "@material-ui/icons/FileCopy"
 import GitHubIcon from "@material-ui/icons/GitHub"
+import IconButton from "@material-ui/core/IconButton"
 import React from "react"
 
 const githubUrl = "https://github.com/skmatz/copitter"
@@ -10,6 +12,9 @@ const Header = () => {
     <div>
       <AppBar position="static">
         <Toolbar>
+          <IconButton color="inherit" edge="start">
+            <FileCopyIcon />
+          </IconButton>
           <Typography variant="h6">Copitter</Typography>
           <div style={{ marginLeft: "auto" }}>
             <a href={githubUrl} target="_blank">
